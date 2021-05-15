4eef00b6e78b2e7983385e0616a36c549ea02f39
Tulio Fernandes <tuliofalmeida@hotmail.com>
2021-05-14 21:20:36 -0300

    add rotinas

--- plx2mat/Matlab Offline Files SDK/lfp_only.m
+++ plx2mat/Matlab Offline Files SDK/lfp_only.m
@@ -129,11 +129,13 @@ for i =1:length(StartingFileName)
             end
         end
     end
+    
     [nev,evnames] = plx_event_names(OpenedFileName);
     
     file_name = string(StartingFileName(i).name).split('.plx');
-    save(string('../Output files/')+string(file_name{1}) + '.mat');
-
+    save_name = string('../Output files/')+string(file_name{1}) + '.mat';
+    save(save_name,'adfreq','adfreqs','adgains','adnames','allad','allts');
+    
     nameFiles2delete(i) = string(StartingFileName(i).name);
 end
 
--- plx2mat/plxRun.py
+++ plx2mat/plxRun.py
@@ -1,10 +1,23 @@
 import os
 import time
 
-print('Este programa converte os arquivos salvos na pasta Input files salvando os arquivos convertidos na pasta Output files')
-print('Entrando na pasta Matlab Offline Files SDK')
-os.chdir('Matlab Offline Files SDK')
-print('Carregando o Matlab e executando o script de conversão')
-os.system('matlab -batch readall')
-print('programa finalizado!!')
+print('_______________________________________________________plx2mat_______________________________________________________')
+print('Este programa converte os arquivos salvos na pasta Input files salvando os arquivos convertidos na pasta Output files.','\n')
+print('Escolha a rotina desejada: ','\n',
+      '1 - converter todos os dados do arquivo.','\n',
+      '2 - converter somente os dados de LFP.','\n')
+print('Entrando na pasta Matlab Offline Files SDK...','\n')
+os.chdir("Matlab Offline Files SDK")
+rotina = int(input("Digite o número da rotina escolhida: "))
+print('Carregando o Matlab e executando o script de conversão...','\n')
+if rotina == 1:
+    os.system('matlab -batch readall')
+    print('Conversão realizada com sucesso!','\n')
+elif rotina == 2:
+    os.system('matlab -batch lfp_only')
+    print('Conversão realizada com sucesso!','\n')
+else:
+    print('A rotina selecionada não existe, reinicie o programa.')
+print('\n')
+print('Programa finalizado.','\n')
 time.sleep(20)
